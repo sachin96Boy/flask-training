6a86e7bb8066d3c24f0d9edd3e25d0d9b0207dac
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-16 08:31:49 +0530

    update: crate sore endpoint

--- store-rest-api/app.py
+++ store-rest-api/app.py
@@ -1,18 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
+
+# lets define a set of dummy stores to return in get request
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
 
 @app.route('/store', methods=['POST'])
 def create_store():
     pass
 
 @app.route('/store/<string:name>')
+# The <> brackets tell Flask that the part of the URL that is inside the brackets is a variable, and that we want to pass it as a parameter to our function.
+# The <string:name> part tells Flask that the variable will be a string.
 def get_store(name):
     pass
 
 @app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
