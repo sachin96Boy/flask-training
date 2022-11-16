bee119349c3e78629c8d68951ee70566ad659cd3
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-16 08:15:26 +0530

    update: iniial routes on store API

--- store-rest-api/app.py
+++ store-rest-api/app.py
@@ -2,8 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
 
 app.run(port=5000)
