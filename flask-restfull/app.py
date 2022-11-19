072653bde53f1dcd0bc52fe4089ca310bb1915ec
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-19 08:26:10 +0530

    update: error control

--- flask-restfull/app.py
+++ flask-restfull/app.py
@@ -8,20 +8,25 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
         request_data = request.get_json()
         item = {'name': name, 'price': request_data['price']}
         items.append(item)
         return item, 201
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 # this endpoint will be /student/<string:name> 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
