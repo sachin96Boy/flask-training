1376c4bf8261d573f8bfd15967ce9e4d77070087
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-19 06:58:44 +0530

    get, post req

--- flask-restfull/app.py
+++ flask-restfull/app.py
@@ -4,12 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        request_data = request.get_json()
+        item = {'name': name, 'price': request_data['price']}
+        items.append(item)
+        return item, 201
+
+
+api.add_resource(Item, '/item/<string:name>')
 # this endpoint will be /student/<string:name> 
 
 app.run(port=5000)
