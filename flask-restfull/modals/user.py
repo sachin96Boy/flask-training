97a5d9f07e9dc06ef8e90b1722bcd79871d47dde
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-27 19:42:14 +0530

    update: sqlalchmy add on user modals and resources

--- flask-restfull/modals/user.py
+++ flask-restfull/modals/user.py
@@ -8,40 +8,19 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, _id, username, password):
+    def __init__(self, username, password):
         # used _id because id is a reserved word in python
-        self.id = _id
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('test.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first() # SELECT * FROM users WHERE username=username LIMIT 1
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('test.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first() # SELECT * FROM users WHERE id=_id LIMIT 1
--- flask-restfull/resources/user.py
+++ flask-restfull/resources/user.py
@@ -14,14 +14,8 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        connection = sqlite3.connect('test.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {"message": "User created successfully."}, 201
 
