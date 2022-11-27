032f8399132b358256827bc8b36fb6f3460017d3
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-11-27 13:45:23 +0530

    update: gt all items

--- flask-restfull/item.py
+++ flask-restfull/item.py
@@ -8,12 +8,6 @@ class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
 
-    @jwt_required()
-    def get(self, name):
-        item = self.find_by_name(name)
-        if item:
-            return item
-        return {'message': 'Item not found'}, 404
 
     @classmethod
     def find_by_name(cls, name):
@@ -29,12 +23,8 @@ class Item(Resource):
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
 
-    def post(self, name):
-        if self.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        request_data = Item.parser.parse_args()
-        item = {'name': name, 'price': request_data['price']}
-        
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('test.db')
         cursor = connection.cursor()
 
@@ -43,6 +33,38 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
+    
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('test.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
+
+    @jwt_required()
+    def get(self, name):
+        try:
+            item = self.find_by_name(name)
+        except:
+            return {"message": "An error occurred finding the item."}, 500
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        request_data = Item.parser.parse_args()
+        item = {'name': name, 'price': request_data['price']}
+        
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
         
         return item, 201
 
@@ -63,14 +85,36 @@ class Item(Resource):
         
         request_data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': request_data['price']}
         if item is None:
-            item = {'name': name, 'price': request_data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}, 500
         else:
-            item.update(request_data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the item."}, 500
+        return updated_item
 
 class ItemList(Resource):
-    def get(self):
+
+    @classmethod
+    def get(cls):
+        connection = sqlite3.connect('test.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
         return {'items': items}
+
+    def get(self):
+        return self.get()
